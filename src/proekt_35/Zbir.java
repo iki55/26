b42d9426ba93a8fa55d98275ef85d833f53fc9c3
iki55 <aleksovska.isidora@gmail.com>
2024-01-12 23:50:49 +0100

    first program

---
+++ src/proekt_35/Zbir.java
@@ -0,0 +1,22 @@
+package proekt_35;
+
+public class Zbir {
+	public static void main(String[]args) {
+
+		Broj b=new Broj();
+
+		b.seta(3);
+
+		b.setb(4);
+
+		int c;
+
+		c=b.geta()+b.getb();
+
+		System.out.println(c);
+
+		}
+
+	}
+
+
